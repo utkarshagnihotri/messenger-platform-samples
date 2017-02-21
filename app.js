e3124858a998669785e3cbb384fe2a1ad105b27b
utkarshagnihotri <utkarsh.agnihotrii@gmail.com>
2017-02-21 13:32:49 +0530

    Adding dependencies

--- app.js
+++ app.js
@@ -945,7 +945,7 @@ const fbMessage = (id, text) => {
     recipient: { id },
     message: { text },
   });
-  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  const qs = 'access_token=' + encodeURIComponent(PAGE_ACCESS_TOKEN);
   return fetch('https://graph.facebook.com/me/messages?' + qs, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
